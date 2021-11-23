7fb74ab468ac74f20f7a6abf65b894c923d01cb7
vultradevs@gmail.com <vultradevs@gmail.com>
2021-11-22 16:46:37 -0800

    Add project files.

---
+++ UpdateChecker/Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace UpdateChecker
+{
+    public class Config : IConfig
+    {
+        [Description("Is this plugin enabled")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Is debug mode enabled? If so, this will spam a lot into your console.")]
+        public bool IsDebugEnabled { get; set; } = true;
+    }
+}
---
+++ UpdateChecker/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+
+namespace UpdateChecker
+{
+    public class Plugin : Plugin<Config>
+    {
+        
+        public override void OnEnabled()
+        {
+            Log.Info("Checking for updates.");
+
+            Log.Debug("Done checking for updates.", Config.IsDebugEnabled);
+        }
+
+        public override void OnDisabled()
+        {
+            Log.Info("Plugin turning off");
+
+        }
+    }
+}
