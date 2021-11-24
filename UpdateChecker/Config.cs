9c6ed3ce1102130669b6bab6dd4375347f3896b0
vultradevs@gmail.com <vultradevs@gmail.com>
2021-11-23 22:05:43 -0800

    v1.0.1 Added checking

--- UpdateChecker/Config.cs
+++ UpdateChecker/Config.cs
@@ -7,6 +7,8 @@ namespace UpdateChecker
     {
         [Description("Is this plugin enabled")]
         public bool IsEnabled { get; set; } = true;
+        [Description("If plugin enable it will update the plugin automatically. If not enabled it will just tell you it needs a update.")]
+        public bool AutoUpdate { get; set; } = true;
         [Description("Is debug mode enabled? If so, this will spam a lot into your console.")]
         public bool IsDebugEnabled { get; set; } = true;
     }
--- UpdateChecker/Plugin.cs
+++ UpdateChecker/Plugin.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Exiled.API.Features;
+using Newtonsoft.Json;
 
 namespace UpdateChecker
 {
     public class Plugin : Plugin<Config>
     {
+
+        public string TagVersion = "v1.0.1";
         
+        private string GetReleases()
+        {
+            const string GITHUB_API = "https://api.github.com/repos/VultraDevs/UpdateChecker/releases";
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("User-Agent", "Unity web player");
+            Uri uri = new Uri(string.Format(GITHUB_API, "VultraDevs", "UpdateChecker"));
+            string releases = webClient.DownloadString(uri);
+            return releases;
+        }
+
+
         public override void OnEnabled()
         {
             Log.Info("Checking for updates.");
+            if (GetReleases().Contains(TagVersion)) 
+            {
+                Log.Info("You're on the latest version.");
 
+            } else
+            {
+                string DownloadURL = "https://github.com/VultraDevs/UpdateChecker/releases/download/+ " + $"{TagVersion}" + "/UpdateChecker.dll";
+                //string DownloadURL = $"https://github.com/VultraDevs/UpdateChecker/releases/download/" + $"{TagVersion}" + "/UpdateChecker.dll";
+                Log.Info("You're not on the latest version. Going to auto update for you if you have the config set.");
+                File.Delete(Paths.Plugins + "/UpdateChecker.dll");
+                var fileName = Path.Combine(Paths.Plugins, "UpdateChecker.dll");
+                Log.Debug("Going to pass through a webclient to download the latest version.", Config.IsDebugEnabled);
+                using (var client = new WebClient())
+                {
+
+                    client.DownloadFile(DownloadURL, fileName);
+                }
+                Log.Debug("Server restart needed to take effect", Config.IsDebugEnabled);
+                
+            }
             Log.Debug("Done checking for updates.", Config.IsDebugEnabled);
         }
 
